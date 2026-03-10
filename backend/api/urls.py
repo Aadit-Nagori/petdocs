a447d58a10742aab241fa6af51543ad4266d17b1
Aadit Nagori <51096338+Aadit-Nagori@users.noreply.github.com>
2026-03-10 01:26:17 -0400

    added template htmls and setup urls for pet views

---
+++ backend/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = "pets"
+urlpatterns = [
+    path("", views.pet_list, name="pet_list"),
+    path("add/", views.PetCreate.as_view(), name="pet_create"),
+    path("<int:pk>/", views.pet_detail, name="pet_detail"),
+    path("<int:pk>/edit/", views.PetUpdate.as_view(), name="pet_update"),
+    path("<int:pk>/delete/", views.PetDelete.as_view(), name="pet_delete")
+]
